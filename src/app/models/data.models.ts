c9035b320a2cc7395f20d78a6696b0d8c86b3387
RawRo <r.tobo@me.com>
2022-03-11 17:58:59 -0500

    minor data flexibility updates

---
+++ src/app/models/data.models.ts
@@ -0,0 +1,13 @@
+export class Employment {
+  code: string;
+  company: string;
+  title: string;
+  start: string;
+  end: string;
+  tasks: string[];
+}
+
+export class Skill {
+  category: string;
+  skills: string[];
+}
